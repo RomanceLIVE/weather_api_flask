667366c95779f522e7658371d80652a5c0f9395d
Robert Horvat <robert.horvath93@gmail.com>
2023-09-17 23:48:51 +0300

    Static test

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
-@app.route("/home")
+
+
+@app.route("/")
 # add /home to the url (http://127.0.0.1:5000/) to see the home page
 def home():
 
 
-    return render_template("sample.html")
+    return render_template("home.html")
     #flask is configured to look for templates in the templates folder
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 10
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 #this will allow to see errors on the website
