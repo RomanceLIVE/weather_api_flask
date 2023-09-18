e484e3fe14e64fec85b94c302eda75cbf4260ab9
Robert Horvat <robert.horvath93@gmail.com>
2023-09-18 22:40:07 +0300

    Updated with Jupyter data

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
 
 
 
 @app.route("/")
 # add /home to the url (http://127.0.0.1:5000/) to see the home page
 def home():
-
-
     return render_template("home.html")
-    #flask is configured to look for templates in the templates folder
+    # flask is configured to look for templates in the templates folder
+
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 10
+    filename = "data_small/TG_STAID" + str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
-#this will allow to see errors on the website
+    app.run(debug=True, port=5000)
+    # change port to avoid port conflict
+
+# this will allow to see errors on the website
